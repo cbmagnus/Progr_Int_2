559bb03b7e21efd06589c1e4546fadb3e49bda5f
cbmagnus <darlan.evaldt@gmail.com>
2015-09-18 19:43:07 -0300

    Lista estoque funcionando e incluir produtos também

--- src/java/Controle/ControladorMB.java
+++ src/java/Controle/ControladorMB.java
@@ -9,7 +9,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import org.apache.jasper.tagplugins.jstl.ForEach;
 
 
 @ManagedBean
